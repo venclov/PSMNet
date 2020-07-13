ca4923f37df4515cade04ad80291a75e20891869
pv819 <paulius.venclovas19@imperial.ac.uk>
2020-07-13 19:24:22 +0100

    batch size 3

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ all_left_img, all_right_img, all_left_disp, test_left_img, test_right_img, test_
 
 TrainImgLoader = torch.utils.data.DataLoader(
          DA.myImageFloder(all_left_img,all_right_img,all_left_disp, True), 
-         batch_size= 1, shuffle= True, num_workers= 8, drop_last=False)
+         batch_size= 3, shuffle= True, num_workers= 8, drop_last=False)
 
 TestImgLoader = torch.utils.data.DataLoader(
          DA.myImageFloder(test_left_img,test_right_img,test_left_disp, False), 
